66ce46500353cad1ccfe4e9670f01df05f305bb1
Shann <shanchh0620@gmail.com>
2024-09-15 16:47:36 +0800

    簡化script.py腳本內容

--- script.py
+++ script.py
@@ -13,40 +13,32 @@ def check_script():
     time.sleep(1.5)
     
     # 選擇裝備品質
-    pyautogui.moveTo(1057, 492)
-    pyautogui.click()
-    time.sleep(random_dealy())
-    pyautogui.moveTo(1055, 536)
-    pyautogui.click()
+    click_event(1057, 492)
+    click_event(1055, 536)
 
     for i in range (4):
 
-        time.sleep(random_dealy())
-        pyautogui.moveTo(tier_default[0], tier_default[1])
-        pyautogui.click()
+        click_event(tier_default[0], tier_default[1])
 
-        time.sleep(random_dealy())
-        pyautogui.moveTo(tier_t5[0], tier_t5[1])
+        click_event(tier_t5[0], tier_t5[1])
         tier_t5[1] += interval
-        pyautogui.click()
 
         enchant_e0 = [795, 535]
 
         for j in range(3):
 
-            time.sleep(random_dealy())
-            pyautogui.moveTo(enchant_default[0], enchant_default[1])
-            pyautogui.click()
+            click_event(enchant_default[0], enchant_default[1])
 
-            time.sleep(random_dealy())
-            pyautogui.moveTo(enchant_e0[0], enchant_e0[1])
+            click_event(enchant_e0[0], enchant_e0[1])
             enchant_e0[1] += interval
-            pyautogui.click()
 
-    pass
+def click_event(x, y):
+    time.sleep(random_dealy())
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
 
 def random_dealy():
     random_number = random.uniform(0.001, 0.7)
     return random_number
 
-# check_script()
+check_script()

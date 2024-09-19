54e711f98314b39fb965d707aa3d71db4978b419
Shann <shanchh0620@gmail.com>
2024-09-19 14:28:18 +0800

    新增裝備list.py進度1/6

---
+++ equip/list.py
@@ -0,0 +1,54 @@
+EquipArmorList = [
+    'Cloth Armor',
+    'Cloth Helmet',
+    'Cloth Shoes',
+    'Leather Armor',
+    'Leather Helmet',
+    'Leather Shoes',
+    'Plate Armor',
+    'Plate Helmet',
+    'Plate Shoes'
+]
+
+EquipMagicList = [
+    'Arcane Staff',
+    'Cursed Staff',
+    'Fire Staff',
+    'Frost Staff',
+    'Holy Staff',
+    'Nature Staff',
+    'Shapeshifter Staff'
+]
+
+EquipMeleeList = [
+    ['Axe', 'Battleaxe', 2, 'Planks', 8, 'MetalBar', 16]
+    ['Axe', 'Greataxe', 2, 'Planks', 12, 'MetalBar', 20],
+    ['Axe', 'Halberd', 2, 'Planks', 20, 'MetalBar', 12],
+    ['Dagger', 'Dagger', 2, 'MetalBar', 12, 'Leather', 12],
+    ['Dagger', 'Dagger Pair', 2, 'MetalBar', 16, 'Leather', 16],
+    ['Dagger', 'Claws', 2, 'MetalBar', 12, 'Leather', 20],
+    ['Hammer', 'Hammer', 1, 'MetalBar', 24],
+    ['Hammer', 'Polehammer', 2, 'MetalBar', 20, 'Cloth', 12],
+    ['Hammer', 'Great Hammer', 2, 'MetalBar', 20, 'Cloth', 12],
+    ['WarGloves', 'Brawler Gloves', 2, 'MetalBar', 12, 'Leather', 20],
+    ['WarGloves', 'Battle Bracers', 2, 'MetalBar', 12, 'Leather', 20],
+    ['WarGloves', 'Spiked Gauntlets', 2, 'MetalBar', 12, 'Leather', 20],
+    # ['Mace'],
+    # ['Mace'],
+    # ['Mace'],
+    # ['Quarterstaff'],
+    # ['Quarterstaff'],
+    # ['Quarterstaff'],
+    # ['Spear'],
+    # ['Spear'],
+    # ['Spear'],
+    # ['Sword'],
+    # ['Sword'],
+    # ['Sword']
+]
+
+
+EquipRangedList = [
+    'Bow',
+    'Crossbow',
+]
